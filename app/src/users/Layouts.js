6f1d8c2363b7db9c11c243430081d677a21a9e34
Jiaxin Lin <craiglin1992@gmail.com>
2016-02-06 17:17:11 -0600

    more

--- app/src/users/Layouts.js
+++ app/src/users/Layouts.js
@@ -42,8 +42,13 @@ function createComparison () {
     replace: true,
     scope: true,
     controllerAs: 'card',
-    controller: function () {
+    controller: function ($scope) {
+        $scope.labels =["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
 
+        $scope.data = [
+          [65, 59, 90, 81, 56, 55, 40],
+          [28, 48, 40, 19, 96, 27, 100]
+        ];
     },
     link: function ($scope, $element, $attrs) {
 
